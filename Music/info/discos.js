56d4e9eae00490091ffaddc81f471f3601e6752e
tulio411 <143910447+tulio411@users.noreply.github.com>
2024-06-08 16:16:27 -0600

    actualizar repo

--- Music/info/discos.js
+++ Music/info/discos.js
@@ -3,34 +3,50 @@ var artistas = ["Alberto",
                 "Alex",
                 "Amaral",                
                 "Ana",
+                "Armin",
                 "Arturo",
+                "Arvo",
                 "Avicii",
                 "Avril",
+                "Bach",
+                "Belinda",
+                "Bizarrap",
                 "Calvin",
                 "Cartel",
                 "Christopher",
                 "Disclosure",
                 "Ellie",
+                "Enrique",
                 "Fatoumata",
+                "Genitallica",
                 "Geoffrey",
                 "Handel",
+                "Hombres",
                 "Illapu",
                 "Indila",
                 "Karol",
                 "Kesha",
                 "Kudai",
+                "LaMaldita",
                 "La",
                 "LaQuinta",
+                "Mendelssohn",
                 "Mercedes",
                 "Metallica",
+                "Molotov",
+                "Mozart",
                 "November",
                 "Orquesta",
                 "Oumou",
+                "Pantera",
+                "Pink",
                 "Pxndx",
                 "Rosalia",
                 "Shakira",
                 "Sia",
+                "Sibelius",
                 "Tlen",
+                "Verdi",
                 "Victor",
                 "Violeta"
             ];
@@ -39,33 +55,49 @@ var dAlberto = ["Cumbia gabacha"]; var fAlberto = [1];
 var dAlex = ["¿Qué pides tú?"]; var fAlex = [1];
 var dAmaral = ["Pájaros en la cabeza"];var fAmaral = [0];
 var dAna = ["Las 12"];var fAna =[1];
+var dArmin = ["Drowning"]; var fArmin = [0];
 var dArturo = ["Fandango at the wall"];var fArturo = [0];
+var dArvo = ["Fratres","Lamentate: Fragile e conciliante"]; var fArvo = [0,1];
 var dAvicii = ["True","Stories", "TIM"];var fAvicii = [0,0,0];
-var dAvril = ["Let go"];var fAvril = [0];
+var dAvril = ["Let go","The Best Damn Thing"];var fAvril = [0,0];
+var dBach = ["Partita para violín 2 en re menor","Sonata para violín 3 en do mayor","Magnificat"];var fBach = [0,0,0];
+var dBelinda = ["Utopia 2"]; var fBelinda = [0];
+var dBizarrap = ["Quevedo: Bzrp Music Sessions, Vol. 52"]; var fBizarrap = [1];
 var dCalvin = ["Motion"];var fCalvin =  [0];
 var dCartel = ["Shorty party"];var fCartel =  [1];
 var dChristopher = ["Hollow Knight"];var fChristopher = [0];
 var dDisclosure = ["Energy"];var fDisclosure = [0];
 var dEllie = ["Halcyon Days"];var fEllie = [0];
+var dEnrique = ["Sex and love"]; var fEnrique = [0];
 var dFatoumata = ["Nterini - A COLORS SHOW"];var fFatoumata = [1];
+var dGenitallica = ["Sean Todos Bien.Venidos"]; var fGenitallica = [0];
 var dGeoffrey = ["Exile"];var fGeoffrey =  [0];
 var dHandel = ["Oratorio del mesías"];var fHandel = [0];
-var dIllapu = ["Música andina"];var fIllapu = [0];
+var dHombres = ["Desayuno continental"]; var fHombres = [0];
+var dIllapu = ["Música andina","Vuelvo amor, vuelvo vida"];var fIllapu = [0,0];
 var dIndila = ["Mini world"];var fIndila = [0];
 var dKarol = ["TQG"];var fKarol = [1];
 var dKesha = ["Animal"]; var fKesha = [0];
 var dKudai = ["Nadha"]; var fKudai = [0];
-var dLa = ["Lo que te conté mientras te hacías la dormida","El viaje de Copperpot", "A las cinco en el Astoria"]; var fLa = [1,1,0];
+var dLaMaldita = ["El circo"]; var fLaMaldita = [0];
+var dLa = ["Lo que te conté mientras te hacías la dormida","El viaje de Copperpot", "A las cinco en el Astoria","Guapa"]; var fLa = [1,1,0,0];
 var dLaQuinta = ["Flores de alquiler"];var fLaQuinta = [0];
+var dMendelssohn = ["Sinfonías"]; var fMendelssohn = [0];
 var dMercedes = ["40 obras fundamentales"];var fMercedes = [0];
 var dMetallica = ["Kill'em all","Master of puppets"]; var fMetallica = [1,1];
+var dMolotov = ["Dance and dense denso","Agua maldita"]; var fMolotov = [0,0];
+var dMozart = ["Adagio para violín y orquesta","Conciertos de violín"]; var fMozart = [1,0];
 var dNovember = ["bedroom walls : le salon"];var fNovember = [0];
 var dOrquesta = ["Huapango de Moncayo"]; var fOrquesta = [0];
 var dOumou = ["Mogoya"]; var fOumou = [0];
+var dPantera = ["The great southern trendkill"]; var fPantera = [0];
+var dPink = ["The wall"]; var fPink = [0];
 var dPxndx = ["Para ti con desprecio","Amantes sunt amentes"]; var fPxndx = [0,0];
 var dRosalia = ["Motomami+"]; var fRosalia = [0];
-var dShakira = ["Laundry service"]; var fShakira = [0];
+var dShakira = ["Laundry service", "Grandes Exitos"]; var fShakira = [0,0];
 var dSia = ["This is acting", "1000 forms of fear"]; var fSia = [0,0];
+var dSibelius = ["Concierto para violín en re menor"]; var fSibelius = [0];
 var dTlen = ["Veracruz Son y Huapango"]; var fTlen = [0];
+var dVerdi = ["Nabucco"]; var fVerdi = [0];
 var dVictor = ["Plegaria a un labrador"];var fVictor = [1];
 var dVioleta = ["El Folklore y la Pasión"]; var fVioleta = [0];
--- Music/info/rolas.js
+++ Music/info/rolas.js
@@ -2,13 +2,24 @@ var Alex0 = ["¿Qué pides tú?","A gritos de esperanza","¿Sabes?","No te rinda
 
 var Amaral0 = ["El universo sobre mi","Días de verano","Revolución", "Mi alma perdida","Marta, Sebas, Guille y los demás","Esta madrugada", "Big Bang", "Enamorada","Tarde para cambiar","En el río"];
 
+var Armin0 = ["Drowning - Avicii Remix"];
+
 var Arturo0 = ["Somos sur"];
+
+var Arvo0 = ["Fratres (Cuerdas)"];
 
 var Avicii0 = ["Wake me up","You make me","Hey brother","Addicted to you","Liar liar","Shame on me","Dear Boy", "EDOM"];
 var Avicii1 = ["Waiting for love","Talk to myself","Touch me","For a better day","Pure grinding","Gonna love ya"];
 var Avicii2 = ["Heaven","SOS (feat. Aloe Blacc)","Peace of mind (feat. Vargas & Lagola)","Tough love (feat. Agnes, Vargas & Lagola)","Bad reputation (feat. Joe Janiak)","Fades away (feat. Noonie Bao)"];
 
-var Avril0 = ["Losing grip","Complicated","Sk8er boi","I'm with you"];
+var Avril0 = ["Losing grip","Complicated","Sk8er boi","I'm with you","Anything but ordinary"];
+var Avril1 = ["Innocence"];
+
+var Bach0 = ["IV Gigue"];
+var Bach1 = ["I Adagio"];
+var Bach2 = ["I Magnificat anima mea Dominum"];
+
+var Belinda0 = ["Luz sin gravedad"];
 
 var Calvin0 = ["Under Control (feat. Hurts)","Blame (feat, John Newman)","Love Now (feat. All About She)","Outside (feat. Ellie Goulding)", "Summer"];
 
@@ -18,28 +29,45 @@ var Disclosure0 = ["Douha (Mali Mali) (Edit)"];
 
 var Ellie0 = ["Burn"];
 
-var Geoffrey0 = ["Land of Anaka","Ye ye ye","Makambo","Solitude","Exile"];
+var Enrique0 = ["Bailando - Spanish version"];
+
+var Genitallica0 = ["Guardaespaldas"];
+
+var Geoffrey0 = ["Land of Anaka","Piri Wango Iya","Ye ye ye","Makambo","Solitude","Exile"];
 
 var Handel0 = ["Part 1: Symphony","Part 1: Comfort ye, My people.. Ev'ry valley shall be exalted","HWV 56 / Pt. 1: And the glory of the lord","HWV 56 / Pt. 1: For unto us a child is born","HWV 56 / Pt. 1: Glory to god in the highest","HWV 56 / Pt. 2: Hallelujah"];
 
+var Hombres0 = ["El secreto de vivir"];
+
 var Illapu0 = ["Canción y huayno","Carnaval","Milonga para nuestros tiempos","Estudio para charango","Tarkeadas","Flor del desierto","Manos obreras","Latinoamérica en quena"];
+var Illapu1 = ["Vuelvo para vivir"];
 
-var Indila0 = ["Dernière danse","Tourner dans le vide","Love story","S.O.S.","Comme un bateau","Run run"];
+var Indila0 = ["Dernière danse","Tourner dans le vide","Love story","S.O.S.","Comme un bateau","Run run", "Ego","Boite en argent","Tu Ne M'Entends Pas"];
 
-var Kesha0 = ["Tik Tok","Take it off","Kiss N Tell"];
+var Kesha0 = ["Tik Tok","Take it off","Kiss N Tell","Blah Blah Blah (feat. 3OH!3)"];
 
 var Kudai0 = ["Morir de amor"];
 
+var LaMaldita0 = ["Solín"];
+
 var La0 = ["Puedes contar conmigo","20 de enero","Rosas","Deseos de cosas imposibles","Geografía","Un mundo mejor","Tú y yo","La esperanza debida","Vestido azul","Adios","Perdóname","La paz de tus ojos","Nadie como tú","Historia de un sueño","Bonustrack"];
 var La1 = ["Cuídate", "Soledad", "París", "La playa","Pop","Dicen que dicen", "Mariposa","La chica del gorro azul","Tu pelo","Tantas cosas que contar","Los amantes del círculo polar",  "Desde el puerto"];
-var La2 = ["El último vals","Inmortal","Jueves","Cumplir un año menos","Palabras para Paula"]
+var La2 = ["El último vals","Inmortal","Jueves","Cumplir un año menos","Palabras para Paula"];
+var La3 = ["Muñeca de trapo","Dulce locura"];
 
 var LaQuinta0 = ["El sol no regresa","Esperaré despierta","Daría","Algo más","Flores de alquiler","Mi ciudad","Si yo fuera mujer"];
+
+var Mendelssohn0 = ["Sinfonía 3 (escocesa), I Andante con moto - Allegro un poco agitato - Assai animato - Andante come prima"];
 
 var Mercedes0 = ["Viva Jujuy"];
 
 var Metallica0 = ["Hit the Lights","The Four Horsemen","Motorbreath","Jump in the Fire","(Anesthesia) – Pulling Teeth","Whiplash","Phantom Lord","No Remorse","Seek & Destroy","Metal Militia"];
 var Metallica1 = ["Battery","Master of Puppets","The Thing That Should Not Be","Welcome Home (Sanitarium)","Disposable Heroes","Leper Messiah","Orion","Damage, Inc."];
+
+var Molotov0 = ["Chandwich a la chichona"];
+var Molotov1 = ["Quién se enoja pierde"];
+
+var Mozart1 = ["Concierto 4, I Allegro", "Concierto 5, III Rondeau. Tempo di menuetto"];
 
 var November0 = ["Corazón caramelo"];
 
@@ -47,17 +75,26 @@ var Orquesta0 = ["Popurrí Sax","El payaso","El trompo","Flor morena","Huapango 
 
 var Oumou0 = ["Bena bena","Djoukourou","Mogoya"];
 
+var Pantera0 = ["10's"];
+
+var Pink0 = ["Goodbye Blue Sky"];
+
 var Pxndx0 = ["Mi huracán llevaba tu nombre","Cita en el quirófano","...Y de la gasolina renació el amor","Hasta el final"];
 var Pxndx1 = ["Narcisista por excelencia","Procedimientos para llegar a un común acuerdo","Los malaventurados no lloran"];
 
 var Rosalia0 = ["Despechá","Motomami"];
 
 var Shakira0 = ["Suerte","Whenever, wherever","Te aviso, te anuncio","Objection"];
+var Shakira1 = ["Estoy aqui"];
 
 var Sia0 = ["Unstoppable","Cheap thrills"];
 var Sia1 = ["Chandelier"];
 
+var Sibelius0 = ["III Allegro ma non tanto"];
+
 var Tlen0 = ["La huasanga","La leva","El fandanguito","El querreque","La sirena","La malagueña","La huastequita","El arreo","La cigarra","El cascabel","La bruja","La tienda","La pollitos","El pijul","El coconito","La bamba"];
+
+var Verdi0 = ["Va, pensiero, sull'ali dorate"];
 
 var Violeta0 = ["La jardinera"];
 
